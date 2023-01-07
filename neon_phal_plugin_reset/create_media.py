8fc7e5dd1441ec35a025826fd25c709c361f0f17
Daniel McKnight <daniel@neongecko.com>
2023-01-06 19:20:52 -0800

    Add helpers to create boot media WIP Add bus handlers for update skill interactions

---
+++ neon_phal_plugin_reset/create_media.py
@@ -0,0 +1,86 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2022 Neongecko.com Inc.
+# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
+# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
+# BSD-3 License
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+from tempfile import mkstemp
+
+import psutil
+
+from subprocess import run, PIPE, Popen
+from os import close
+from os.path import exists
+from typing import List, Optional
+
+import requests
+from ovos_utils.log import LOG
+
+
+def get_drive_mountpoints(drive: str = "/dev/sdb") -> List[str]:
+    """
+    Get a list of all mountpoints associated with a disk device.
+    """
+    parts = psutil.disk_partitions(all=False)
+    valid_parts = [part for part in parts
+                   if part.device.startswith(drive)]
+    LOG.debug(valid_parts)
+    return [part.mountpoint for part in valid_parts]
+
+
+def prep_drive_for_write(drive: str = "/dev/sdb"):
+    if not exists(drive):
+        LOG.warning(f"Drive doesn't exist: {drive}")
+        return
+
+    for mp in get_drive_mountpoints(drive):
+        run(["/usr/bin/umount", mp])
+
+
+def write_xz_image_to_drive(image_path: str, drive: str = "/dev/sdb"):
+    """
+    Write an image to a USB drive
+    """
+    image_data = Popen(["/usr/bin/xzcat", image_path], stdout=PIPE)
+    run(["/usr/bin/dd", f"of={drive}"], stdin=image_data.stdout)
+    LOG.info("Drive creation completed")
+
+
+def download_image(image_url: str = None) -> Optional[str]:
+    """
+    Download an image file from the specified URL
+    """
+    image_url = image_url or "https://2222.us/app/files/neon_images/pi/" \
+                             "mycroft_mark_2/recommended_mark_2.img.xz"
+    LOG.debug(f"Downloading {image_url}")
+    request = requests.get(image_url)
+    LOG.debug(f"Downloaded Completed")
+    if not request.ok:
+        LOG.error(request.text)
+        return
+    fp, tmpfile = mkstemp()
+    close(fp)
+    with open(tmpfile, 'wb') as f:
+        f.write(request.content)
+    return tmpfile
