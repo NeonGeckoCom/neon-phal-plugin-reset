d6ea89b9a25c2dff72f9715609fb27e3f1c14ab1
Daniel McKnight <daniel@neongecko.com>
2023-05-15 13:32:58 -0700

    Update GH Automation to latest best practices

--- setup.py
+++ setup.py
@@ -42,38 +42,30 @@ class CustomInstall(install):
 
 
 PLUGIN_ENTRY_POINT = "neon-phal-plugin-reset=neon_phal_plugin_reset:DeviceReset"
-BASEDIR = path.abspath(path.dirname(__file__))
+BASE_PATH = path.abspath(path.dirname(__file__))
 
 
-with open("./neon_phal_plugin_reset/version.py", "r", encoding="utf-8") as v:
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(BASE_PATH, "requirements",
+                                  requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.strip() for r in requirements if r.strip() and
+                    not r.strip().startswith("#")]
+    return requirements
+
+
+with open(path.join(BASE_PATH, "README.md"), "r") as f:
+    long_description = f.read()
+
+with open(path.join(BASE_PATH, "neon_phal_plugin_reset", "version.py"), "r",
+          encoding="utf-8") as v:
     for line in v.readlines():
         if line.startswith("__version__"):
             if '"' in line:
                 version = line.split('"')[1]
             else:
                 version = line.split("'")[1]
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-
-def get_requirements(requirements_filename: str):
-    requirements_file = path.join(path.abspath(path.dirname(__file__)),
-                                  "requirements", requirements_filename)
-    with open(requirements_file, 'r', encoding='utf-8') as r:
-        requirements = r.readlines()
-    requirements = [r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")]
-
-    for i in range(0, len(requirements)):
-        r = requirements[i]
-        if "@" in r:
-            parts = [p.lower() if p.strip().startswith("git+http") else p for p in r.split('@')]
-            r = "@".join(parts)
-            if getenv("GITHUB_TOKEN"):
-                if "github.com" in r:
-                    r = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
-            requirements[i] = r
-    return requirements
 
 
 setup(
